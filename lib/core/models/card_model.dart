6cf1369879f4ea8f542b11041c502b031308e36b
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-04 00:22:50 +0300

    Implement Hive adapter and annotations for CardModel

--- lib/core/models/card_model.dart
+++ lib/core/models/card_model.dart
@@ -1,8 +1,22 @@
-class CardModel {
+import 'package:hive/hive.dart';
+
+part 'card_model.g.dart';
+
+@HiveType(typeId: 0)
+class CardModel extends HiveObject {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String? category;
+
+  @HiveField(2)
   final String front;
+
+  @HiveField(3)
   final String? hint;
+
+  @HiveField(4)
   final String back;
 
   CardModel({
