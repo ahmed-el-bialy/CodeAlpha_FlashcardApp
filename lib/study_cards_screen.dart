3a6d8ead86985f762286118612b6d8d5afae6cdf
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-05 23:17:08 +0300

    Implement StudyCardsScreen with AppBar and GridView of FlashCards

--- lib/study_cards_screen.dart
+++ lib/study_cards_screen.dart
@@ -1,5 +1,8 @@
+import 'package:code_alpha_flash_card_app/core/models/card_model.dart';
 import 'package:code_alpha_flash_card_app/core/theming/app_colors.dart';
+import 'package:code_alpha_flash_card_app/features/home/ui/widgets/flash_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class StudyCardsScreen extends StatelessWidget {
   const StudyCardsScreen({super.key});
@@ -9,6 +12,31 @@ class StudyCardsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.darkBackground,
 
+        appBar: AppBar(
+          elevation: 1,
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Study Cards",
+            style: TextStyle(color: AppColors.indigoAccent, fontSize: 19.sp),
+          ),
+          centerTitle: true,
+          iconTheme: IconThemeData(
+              color: AppColors.indigoAccent
+          ),
+        ),
+
+        body: SafeArea(child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                childAspectRatio: 1.5,
+                mainAxisSpacing: 4.h),
+            itemBuilder: ((context, index) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 8.h),
+                child: FlashCard(cardModel: CardModel(
+                    id: "5", front: "what is that??", back: "Nothing")),
+              );
+            })))
     );
   }
 }
