138f610f156d5001438289db0d50a76c57ecfec2
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-05 21:12:10 +0300

    add gray color to app_colors.dart

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -5,6 +5,7 @@ class AppColors {
   static const Color indigoAccent = Color(0xFF818CF8);
   static const Color iceBlue = Color(0xFFDAE2FD);
   static const Color lavenderGray = Color(0xFFC6C5D5);
+  static const Color gray = Color(0xFF9CA3AF);
   static const Color oceanBlue = Color(0xFF1E293B);
   static const Color accentCyan = Color(0xFF00D1FF);
 }
